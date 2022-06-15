51d2657aa4e28cc4b14ded99bf96a83f08f72751
“Blood421” <motegireiujiutuho421@yahoo.co.jp>
2022-06-15 19:29:54 +0900

    UITimer関係のスクリプト実装

---
+++ Assets/Scripts/UI/Motegi/MainGame/Timer/UITimerSystem.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITimerSystem : MonoBehaviour
+{
+    [SerializeField] private UITimerModel uiTimerModel;
+    [SerializeField] private float maxTime = 60;
+    [SerializeField] private bool isTimerOn = false;
+    private float time = 0;
+
+    private void Awake()
+    {
+        time = maxTime;
+        uiTimerModel.SetTimerValue(time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimerOn)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                time = 0;
+                TimerStop();
+                uiTimerModel.SetTimerValue(time);
+                uiTimerModel.TimerComplete();
+            }
+            else
+            {
+                uiTimerModel.SetTimerValue(time);
+            }
+        }
+    }
+    public void TimerStop()
+    {
+        isTimerOn = false;
+    }
+
+    public void TimerStart()
+    {
+        isTimerOn = true;
+    }
+}
