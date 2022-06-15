65bb9e8a87bc28b27f5d0a4870bc5ee9702d1e64
Daisuke Tsukada <dsgarage@gmail.com>
2022-06-15 16:59:26 +0900

    FarmEngine改修

--- Assets/Script/FarmEngine.cs
+++ Assets/Script/FarmEngine.cs
@@ -1,10 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+
+
+public enum Achievement
+{
+    Seed,
+    Corn,
+    Potatoes,
+    Tomato,
+    Watermelon,
+}
 
 public class FarmEngine : MonoBehaviour
-{
-    // メンバー、成長パラメーター
-    private ;
+{ 
+    [SerializeField] private Achievement achievement;
+    [SerializeField] private bool growth = false;
+    [SerializeField] private bool rotten = false;
+    [SerializeField] private int maxDegreeOfGrowth = 10;
+    
+    private int _degreeOfGrowth = 0;
 
+    private void Init()
+    {
+        growth = false;
+        rotten = false;
+        _degreeOfGrowth = 0;
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Achievement Harvest()
+    {
+        if (growth)
+        {
+            return achievement;
+            Init();
+        }
+        else
+        {
+            achievement = Achievement.Seed;
+        }
+    
+        return achievement;
+    }
+
+    public void Destruction()
+    {
+        
+    }
+    
 }
