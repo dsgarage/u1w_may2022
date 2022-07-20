b27982a23adb14ac6ea81d73ebdcde5fb962e5b0
fsfujinity <huishitengzhi50@gmail.com>
2022-07-20 16:25:09 +0900

    拾うアニメーションを追加しました。

--- Assets/Script/PlayercontrollerMouseRotateControlle.cs
+++ Assets/Script/PlayercontrollerMouseRotateControlle.cs
@@ -13,70 +13,24 @@ public class PlayercontrollerMouseRotateControlle : MonoBehaviour
     public float gravity = 20.0F;       //重力の大きさ
     public float rotateSpeed = 3.0F;    //回転速度
 
-    private Animator anim;
     private CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
     private float _moveHorizon;
     private float _moveVertical;
     private float _mouseX;
+
+
     
     void Start () {
-        anim = gameObject.GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
         
     }
 
     void Update () {
 
-        /*else if (Input.GetKeyDown(KeyCode.S))
-        {
-            //transform.Rotate(new Vector3(0, -180, 0));
-            anim.SetBool("blanim", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            anim.SetBool("blanim", false);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            //transform.Rotate(new Vector3(0, 90, 0));
-            anim.SetBool("blanim", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            anim.SetBool("blanim", false);
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            //transform.Rotate(new Vector3(0, -90, 0));
-            anim.SetBool("blanim", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.A))
-        {
-            anim.SetBool("blanim", false);
-        }*/
-
-
-
-
-
         _moveHorizon = Input.GetAxis ("Horizontal");    //左右矢印キーの値(-1.0~1.0)
         _moveVertical = Input.GetAxis ("Vertical");      //上下矢印キーの値(-1.0~1.0)
         _mouseX = Input.GetAxis ("Mouse X");      //マウスの左右移動量(-1.0~1.0)
-
-
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
-        {
-            anim.SetBool("blanim", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
-        {
-            anim.SetBool("blanim", false);
-        }
-
-
-
-
 
 
         //キャラクターの移動を回転
@@ -87,12 +41,11 @@ public class PlayercontrollerMouseRotateControlle : MonoBehaviour
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
         }
+
         
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
         
-
-
 
     }
 }
